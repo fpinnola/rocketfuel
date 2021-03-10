9d2d5c9183a900eed5c1bc663737fcdabddae4b0
Frank Pinnola <frankie4pinnola@gmail.com>
2021-03-09 23:00:21 -0500

    Team photos/info added

--- src/pages/team.js
+++ src/pages/team.js
@@ -2,6 +2,20 @@ import * as React from "react"
 import Layout from "../components/layout";
 import { Link } from "gatsby"
 
+const team = [
+  { imageURL: "brianLi.jpg", name: "Brian Li", title: "E-Board"},
+  { imageURL: "colefelsher.jpg", name: "Cole Felsher", title: "E-Board"},
+  { imageURL: "jonsamuel.jpg", name: "Jon Samuel", title: "E-Board"},
+  { imageURL: "wallistoscarelli.jpg", name: "Wallis Toscarelli", title: "E-Board"},
+  { imageURL: "adamlatif.jpg", name: "Adam Latif", title: "E-Board"},
+  { imageURL: "chelseabraithwaite.jpg", name: "Chelsea Braithwaite", title: "E-Board"},
+  { imageURL: "justinmurray.jpg", name: "Justin Murray", title: "E-Board"},
+  { imageURL: "jocelynragukonis.jpg", name: "Jocelyn Ragukonis", title: "E-Board"},
+  { imageURL: "bryankyritz.jpg", name: "Bryan Kyritz", title: "E-Board"},
+  { imageURL: "frankpinnola.jpg", name: "Frank Pinnola", title: "Launchpad Advisor"},
+  { imageURL: "mukundiyengar.jpg", name: "Mukund Iyengar", title: "Director of Launchpad"},
+]
+
 const pageStyles = {
     color: "#232129",
     width: '100%',
@@ -22,11 +36,58 @@ const pageStyles = {
 
   const container = {
     display: 'flex',
-    padding: 25
+    padding: 25,
+    flexDirection: 'column',
   }
 
   const title = {
     color: "#A32638"
+  }
+
+  const list = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '2em',
+    justifyContent: 'center'
+  }
+
+  const item = {
+    display: 'flex',
+    padding: '0.5em',
+    width: '20%',
+    minWidth: '25ch',
+  }
+  
+
+
+  const TeamMember = function({ imageURl, name, title }){
+
+    var cardStyle = {
+      flexGrow: 1,
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+      width: '100%',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      borderRadius: 12,
+      backgroundImage: `url(${imageURl})`,
+      height: 200,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      overflow: 'hidden'
+    }
+
+    return (
+      <div style={cardStyle}>
+        <div style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.3)', padding: '1em', color: "white", display: 'flex', flexDirection: 'column', justifyContent: "flex-end" }}>
+          <label>{name}</label>
+          <label>{title}</label>
+
+        </div>
+      </div>
+    )
   }
   
   const Team = () => (
@@ -35,6 +96,15 @@ const pageStyles = {
       <main style={pageStyles}>
         <div style={container}>
           <h1 style={title}>Our Team</h1>
+          <ul style={list}>
+            {team.map((member) => 
+              <li style={item}>
+                <TeamMember imageURl={member.imageURL} name={member.name} title={member.title} />
+              </li>
+            )}
+          </ul>
+
+
         </div>
       </main>
     </Layout>
